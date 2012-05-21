783e375c98c6e435172abc775f7fea1ac8460c79
Peter Kordel <pkordel@gmail.com>
2012-05-21 13:44:32 +0200

    Adds basic error classes

--- lib/active_rdf.rb
+++ lib/active_rdf.rb
@@ -2,6 +2,7 @@ require 'active_support'
 require 'active_model'
 require 'active_attr'
 require 'active_rdf/exceptions'
+require 'active_rdf/errors'
 require 'active_rdf/version'
 
 module ActiveRDF

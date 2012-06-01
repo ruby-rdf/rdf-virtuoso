2b2a91dc47bb34960cdf3fdf81a9e3db0e3c32f0
Peter Kordel <pkordel@gmail.com>
2012-06-01 15:51:30 +0200

    Bumped version

--- lib/active_rdf/version.rb
+++ lib/active_rdf/version.rb
@@ -2,7 +2,7 @@ module ActiveRDF
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 0
-    TINY  = 5
+    TINY  = 6
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
--- lib/rdf/virtuoso/version.rb
+++ lib/rdf/virtuoso/version.rb
@@ -1,5 +1,5 @@
 module RDF
   module Virtuoso
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
   end
 end

25157872ca6f5213d13a0985765a5be27b44d9f6
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-04-13 16:33:58 +0100

    control whether the integration tests are run with an env variable #15

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,14 +4,19 @@ $LOAD_PATH.unshift '.'
 require File.join(File.dirname(__FILE__), 'spec_helper')
 require 'rdf/spec/repository'
 
-# these tests rely on a Virtuoso instance being available, at port 8890, and will otherwise fail
+# these tests rely on a Virtuoso service being available, at port 8890
 # the easiest way to do this is with docker ( https://docs.docker.com/get-docker/ ), running the following:
 #   docker run -p 8890:8890 -p 1111:1111 -e DBA_PASSWORD=tester -e SPARQL_UPDATE=true --name virtuoso-testing -d tenforce/virtuoso
 # when finished you can stop with
 #   docker stop virtuoso-testing
+#
+# to avoid the tests being skipped, you will need to set the environment variable VIRTUOSO_INTEGRATION_TESTS, e.g.
+#   VIRTUOSO_INTEGRATION_TESTS=true bundle exec rspec spec
 
-describe RDF::Virtuoso::Repository do
-  context('when interacting with a virtuoso repository instance') do
+skip = ENV['VIRTUOSO_INTEGRATION_TESTS'] ? false : 'Skipping Integration tests against a running repository, see spec/integration_spec.rb'
+
+describe RDF::Virtuoso::Repository, skip: skip do
+  context('when interacting with a virtuoso repository service') do
     subject(:repository) do
       described_class.new(uri,
                           update_uri: update_uri,
@@ -28,7 +33,6 @@ describe RDF::Virtuoso::Repository do
 
     it 'is able to select' do
       # check a single triple result which is unlikely to change
-
       query = RDF::Virtuoso::Query.select.where([RDF::URI('http://localhost:8890/sparql'),
                                                  RDF::URI('http://www.w3.org/ns/sparql-service-description#endpoint'), :o])
 
@@ -47,7 +51,6 @@ describe RDF::Virtuoso::Repository do
     end
 
     # commented out until conformance issues are resolved, otherwise there are many errors
-    # it_behaves_like "an RDF::Repository"        
-
+    # it_behaves_like "an RDF::Repository"
   end
 end
